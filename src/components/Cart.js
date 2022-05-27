8bb809b2e06f835d655b9a983f3259810d9717fe
andrea-maiolo <maiolo.m.2@gmail.com>
2022-05-27 19:02:18 +0200

    working on making the cart like an hamburger button

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,14 +5,31 @@ function Cart(props) {
     console.log("i have been clicked");
     console.log(props.cart);
   }
+
+  const [cartDisplay, setCartDisplay] = React.useState();
+
+  React.useEffect(() => {
+    if (props.cart.length === 0 || props.cart.length === null) {
+      setCartDisplay("Your cart is empty");
+    } else {
+      setCartDisplay();
+    }
+    // let myA = props.cart.map((p) => console.log(p));
+  }, [props.cart]);
+
   return (
-    <button className="cartButton" onClick={handleClick}>
-      <img
-        className="cartImg"
-        alt="Your cart"
-        src={require(`../styles/shopping_cart.svg`).default}
-      />
-    </button>
+    <div>
+      <button className="cartButton" onClick={handleClick}>
+        <img
+          className="cartImg"
+          alt="Your cart"
+          src={require(`../styles/shopping_cart.svg`).default}
+        />
+      </button>
+      <div className="cartVisible">
+        <p>{cartDisplay}</p>
+      </div>
+    </div>
   );
 }
 
